f3a539a0c067293cca992c655d03263a932eb13a
rohann2o2929-prog <rohann2o2929@gmail.com>
2026-08-15 02:33:28 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='sapa-find-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./data/config.js','./data/content.js','./assets/logo.svg','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
